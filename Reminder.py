36d442789cdb25d8b69e480a1401cf45ad8a47b8
Adam Schellenberg <adamschellenberg93@gmail.com>
2022-03-28 21:23:33 -0700

    initial. it begins now.

---
+++ Reminder.py
@@ -0,0 +1,35 @@
+print("Want to read a book? Y or N")
+answer = input()
+if answer.lower() == "y":
+	print("Program first.")
+elif answer.lower() == "n":
+	print("Good. Program.")
+else:
+	print("Program anyways.")
+
+print("Want to read some manga? Y or N")
+answer = input()
+if answer.lower() == "y":
+	print("Program first.")
+elif answer.lower() == "n":
+	print("Good. Program.")
+else:
+	print("Program anyways.")
+	
+print("Want to play a game? Y or N")
+answer = input()
+if answer.lower() == "y":
+	print("Program first.")
+elif answer.lower() == "n":
+	print("Good. Program.")
+else:
+	print("Program anyways.")
+	
+print("Did you program today? Y or N")
+answer = input()
+if answer.lower() == "y":
+	print("Program again.")
+elif answer.lower() == "n":
+	print("Program.")
+else:
+	print("Program.")
